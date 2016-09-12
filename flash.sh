36d777fb585b6cdf3a854cf455225530e2b8cfed
Elf Eldridge <kaiwhata@gmail.com>
2016-09-12 13:28:34 +1200

    Update flash.sh

--- flash.sh
+++ flash.sh
@@ -5,11 +5,12 @@ gpio mode 4 out
 #turn LED on
 gpio write 4 1 &
 #start video capture
-raspivid -o outvid.h264 -t 5000 &
+raspivid -o outvid.h264 -t 60000 &
 #flash neopixel for appropriate time
 sudo python rpi_ws281x/python/examples/neopixel_elf.py &
 
 wait
-gpio write 4 0
+#gpio write 4 0
 echo 'Complete'
 #now call  image processing script on the generated file
+MP4Box -add outvid.h264 outvid.mp4
