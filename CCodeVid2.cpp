1b0743992efa53ced68785b7cd3f68bbb9d093f4
polutekape <kapeteni7@gmail.com>
2016-12-08 04:32:12 +0000

    add updated version of Vid2

--- CCodeVid2.cpp
+++ CCodeVid2.cpp
@@ -1,53 +1,168 @@
 #include <string>
 #include <iostream>
 #include <vector>
-
+#include <fstream>
 #include <ctime>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <raspicam/raspicam_cv.h>
+#include <iomanip>
 
 
 //Prevents naming collisions
 using namespace cv;
 using namespace std;
 
+void getRedColor (Mat frame);
+void getEqualize (Mat frame);
+void getBlur     (Mat frame);
+void getThresh	 (Mat frame);
+void getHist ();
+Mat Image;
+Mat Temp;
+double RedTime = 0;
+double EqualTime = 0;
+double BlurTime = 0;
+double ThreshTime = 0;
 
 int main(int argc, char** argv){
 
+	clock_t Time;
+	Time = clock();
 
 raspicam::RaspiCam_Cv Camera; //Camera Object
 //Open Camera
-//Camera.set(CV_CAP_PROP_FORMAT, CV_8UC1);
-
-cout<<"Opening up the camera"<<endl;
+Camera.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+Camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+ 
 if(!Camera.open()){
 cout<<"Error opening the camera"<<endl;
 return -1;
 }
+	int nCount = 50;
 
-	Mat Image;
-	int nCount = 300;
 	
 //Capturing Frames
 cout<<"Capturing frames"<<endl;
-namedWindow("Display Window", WINDOW_NORMAL);
+//namedWindow("DisplayImage", WINDOW_NORMAL);
+//namedWindow("RedImage", WINDOW_NORMAL);
+//namedWindow("EqualizeImage", WINDOW_NORMAL);
+//namedWindow("BlurImage", WINDOW_NORMAL);
+namedWindow("ThreshImage",WINDOW_NORMAL);
+
+int count = 0;
+//int count2 = 0;
+int x;
+int j;
+int count2 =0;
+
+
+ofstream myfile ("BlackPixels.txt");
 
 for(int i =0; i<=nCount; i++){
-
-	Camera.grab();
+	Camera.grab(); 
 	Camera.retrieve (Image);
-	imshow("Display Window", Image);
-        waitKey(30);	
-	if (i%3==0){cout<<"\r Captured "<<i<<" Images \n"<<endl;}
+	if(i >= 6){
+	getRedColor(Image);
+
+	getEqualize(Image);
+
+    Temp = Image.clone();
+    getBlur(Temp);
+    getThresh(Image);
+	
+	for (x = 0; x<Temp.cols;x++){
+	  for(j = 0;j<Temp.rows;j++){
+		int k=Temp.at<uchar>(j,x);
+                if(k==0){ count2++;}      
+		 }
+	  }
+    cout << count2 <<'\n';
+
+	imshow("ThreshImage",Image);
+	waitKey(1);
+
+	cout<<"frame "<<i<<endl;
+	   for (x = 0; x<Image.cols;x++){
+	for(j = 0;j<Image.rows;j++){
+		int k=Image.at<uchar>(j,x);
+                if(k==0){ count++;}      
+		}
+	} 
+     if (myfile.is_open()){
+	myfile<<i<<" "<<count<<" "<<count2<<'\n';		 
+	}
+  cout<<"Black Pixels : "<<count<<" in frame "<<i<<endl;
+  count = 0;
+  count2 = 0;
+  }
 }
-//waitKey();	
-cout<<"Stop Camera..."<<endl;
-Camera.release();        
+
+ myfile.close();
+
+//cout<<"Red Process Time "<<RedTime<<'\n';
+//cout<<"Equal Process Time "<<EqualTime<<'\n';
+//cout<<"Blur Process Time "<<BlurTime<<'\n';
+//cout<<"Thresh Process Time "<<ThreshTime<<'\n';
+//cout<<"Total "<<(RedTime+EqualTime+BlurTime+ThreshTime)<<'\n';
+//double Subtotal = RedTime+EqualTime+BlurTime+ThreshTime;
+//cout<<"Total Process Time "<<setprecision(3)<<(Subtotal)<<endl;
+//cout<<"Stop Camera..."<<endl;
+Camera.release();
+getHist();        
+  	
+ double Total = (clock() - Time)/(double)CLOCKS_PER_SEC;
+ cout<<"Total Process Time "<<setprecision(3)<<(Total)<<endl;
+     return 0;
+}
+
+void getRedColor(Mat Frame){
+clock_t start;
+start = clock();
+Mat BRG[3];
+split(Frame,BRG);
+Image = BRG[2];
+double s = ((clock()-start)/(double)CLOCKS_PER_SEC);
+RedTime += s;
+}
+
+void getEqualize (Mat Frame){
+clock_t start2;
+start2 = clock();
+equalizeHist(Frame,Image);
+double s2 = ((clock()-start2)/(double)CLOCKS_PER_SEC);
+EqualTime += s2;
+}
+
+void getBlur (Mat Frame){
+clock_t start3;
+start3 = clock();
+medianBlur(Frame,Temp,13);
+double s3 = ((clock()-start3)/(double)CLOCKS_PER_SEC);
+BlurTime += s3;
+}
+
+void getThresh (Mat Frame){
+clock_t start4;
+start4 = clock();
+threshold(Frame,Image,30,175, THRESH_BINARY);
+double s4 = ((clock()-start4)/(double)CLOCKS_PER_SEC);
+ThreshTime += s4;
+}
 
 
-   
-    return 0;
-}
+void getHist(){	
+		FILE *gnuplotPipe = popen ("gnuplot -persistent", "w");
+		if(gnuplotPipe){	
+		fprintf(gnuplotPipe, "plot 'BlackPixels.txt' using 1:2 with lines, 'BlackPixels.txt' using 1:3 with lines \n");
+		fflush(gnuplotPipe);
+				}
+	printf("Displaying graph \n");		
+	}
+
+
+
+
+
 
