2e30ff2c8b854dbf9b6eb322af48d34b0dd7f2c2
polutekape <kapeteni7@gmail.com>
2016-12-14 22:58:39 +0000

    add update

--- CCode.cpp
+++ CCode.cpp
@@ -1,12 +1,12 @@
 #include <string>
 #include <iostream>
 #include <vector>
-
+#include <iomanip>
 #include <ctime>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-//#include <raspicam/raspicam_cv.h>
+
 
 int main(int argc, char** argv)
 {
@@ -18,8 +18,6 @@ int main(int argc, char** argv)
     std::clock_t start;
     double duration;
     start = std::clock();
-
- //  raspicam::RaspiCam_Cv Camera;
 
 
 //Obtain the name of the Input-Image
@@ -35,73 +33,70 @@ int main(int argc, char** argv)
     }   
 
    duration = (std::clock()-A)/(double)CLOCKS_PER_SEC;
-   std::cout<<"Reading Image :"<<duration<<"\n";  
-
+   std::cout<<"Reading Image :"<<std::setprecision(2)<<duration<<"\n";  
+   std::cout<<"Image Size ="<<image.cols<<"x"<<image.rows<<"\n";
+   
     cv::Mat EqualImage;
     cv::Mat BlurImage;
     cv::Mat ThreshImage;
+    cv::Mat ThreshImage2;
 //Variables for computing threshold
-	double thresh = 30;
-	double MaxValue = 175;
+	double thresh = 5;
+	double thresh2 = 10;
+	double MaxValue = 255;
 //Variables for counting black pixels
 	int i;
 	int j;
 	int count=0; 
 
-   // cv::namedWindow(name, cv::WINDOW_AUTOSIZE);
-   // cv::imshow(name, image);
-  //  cv::waitKey();
 
+start = std::clock();
     cv::Mat bgr[3];   //destination array 
     split(image,bgr);//split source (RED COLOUR AT THIS STAGE)
 
-     //Note: OpenCV uses BGR color order in this case only dealing with RED
-    //imwrite("blue.png",bgr[0]); //blue channel
-    // imwrite("green.png",bgr[1]); //green channel
-    // imwrite("red.png",bgr[2]); //red channel
-
+    //Obtain Red Channel
     cv::Mat RedImage = bgr[2];
-    imwrite("RedImage.png",RedImage);
-
-
-
-    cv::namedWindow("Display Window",cv::WINDOW_AUTOSIZE);
-    cv::imshow("Display Window", RedImage);
-	std::cout<<"Display Window"<<std::endl;
-
-    ///Convert to grayscale
-   // cv::cvtColor(Image, Image, CV_BGR2GRAY);
-
-
-         //Apply histogram Equalization
-          cv::equalizeHist(RedImage, EqualImage);
-          cv::imwrite("EqualImage.png", EqualImage);
-      
-         //apply median smoothing      
-          medianBlur (EqualImage, BlurImage,13); 
-          cv::imwrite("BlurImage.png", BlurImage);
-        
-         //Apply Threshold
-        cv::threshold(BlurImage,ThreshImage, thresh, MaxValue,cv::THRESH_BINARY);  
-         cv::imwrite("ThreshImage.png", ThreshImage);      
-            
-         
-        std::cout<<"Image Size ="<<ThreshImage.cols<<"x"<<ThreshImage.rows<<"\n";	
+    //Apply Equalize
+    cv::equalizeHist(RedImage, EqualImage);
+    //Apply medianBlur
+    cv::medianBlur (EqualImage, BlurImage,11);
+    //Apply Threshold
+    cv::threshold(BlurImage,ThreshImage, thresh, MaxValue,cv::THRESH_BINARY);  
+    cv::threshold(ThreshImage,ThreshImage2, thresh2, MaxValue,cv::THRESH_BINARY);                 	
 	
+	//counting black pixels
+	std::clock_t B;
+	B = clock();
      for (i = 0; i<ThreshImage.cols;i++){
 	for(j = 0;j<ThreshImage.rows;j++){
 		int k=ThreshImage.at<uchar>(j,i);
                 if(k==0){ count++;}      
 		}
 	}  
-     
-        std::cout<<"Number of BlackPixels ="<<count<<"\n";  
+    std::cout<<"for loop : "<<(clock()-B)/(double)CLOCKS_PER_SEC<<std::endl;
+    
+    duration = (std::clock() - start)/(double)CLOCKS_PER_SEC;
+    std::cout<<"Number of BlackPixels = "<<count<<"\n";  
 
-	 duration = (std::clock() - start)/(double)CLOCKS_PER_SEC;
-         std::cout<< "Process Time: " << duration <<"\n";
-       //	std::cout<<"Process Time: "<<((std::clock()-start)/(double)CLOCKS_PER_SEC)<<'\n';
-   	 std::cout << "Images have been processed" << std::endl; //success message
+    std::cout<< "Process Time: "<<std::setprecision(2)<< duration <<"\n";
+	std::cout << "Images have been processed" << std::endl; //success message
 
+	cv::namedWindow("Red Image",cv::WINDOW_NORMAL);
+    cv::imshow("Red Image", RedImage);
+    	
+	cv::namedWindow("EqualImage", cv::WINDOW_NORMAL);
+    cv::imshow("EqualImage",EqualImage);
+	
+	cv::namedWindow("BlurImage", cv::WINDOW_NORMAL);
+	cv::imshow("BlurImage", BlurImage);
+
+	cv::namedWindow("ThreshImage",cv::WINDOW_NORMAL);
+	cv::imshow("ThreshImage", ThreshImage);
+	
+	cv::namedWindow("ThreshImage2",cv::WINDOW_NORMAL);
+	cv::imshow("ThreshImage2", ThreshImage2);
+	
+         cv::waitKey();
 
     return 0;
 
--- CCodeVid2.cpp
+++ CCodeVid2.cpp
@@ -17,8 +17,8 @@ using namespace std;
 
 void getRedColor (Mat frame);
 void getEqualize (Mat frame);
-void getBlur     (Mat frame, int R);
-void getThresh	 (Mat frame, Mat Frame2, int R);
+void getBlur     (Mat frame);
+void getThresh	 (Mat frame);
 void getHist ();
 Mat Image;
 Mat Temp;
@@ -28,106 +28,105 @@ double BlurTime = 0;
 double ThreshTime = 0;
 
 //Rates for MedianBlur & Threshold
-
+int MBlurRate = 5;
+int THoldRate = 10;
 
 int main(int argc, char** argv){
 
-int MBlurRate = 31;
-int THoldRate = 5;
 
     clock_t Time;
     Time = clock();
 
     raspicam::RaspiCam_Cv Camera; //Camera Object
     //Open Camera
-    Camera.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+    Camera.set(CV_CAP_PROP_FRAME_WIDTH,640);
     Camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+    
+    //Camera.set(CV_CAP_PROP_FRAME_WIDTH,1280);
+    //Camera.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
+    
+    //Camera.set(CV_CAP_PROP_FRAME_WIDTH,1920);
+    //Camera.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
  
     if(!Camera.open()){
 	cout<<"Error opening the camera"<<endl;
 	return -1;
     }
-    int nCount = 100;
+    
+    //elfs edits to default capture values
+    //Camera.set(CV_CAP_PROP_GAIN, 50) // values range fropm 0 to 100
+    //Camera.set(CV_CAP_PROP_EXPOSURE, 50) //-1 is auto, values range from 0 to 100
+    //Camera.set(CV_CAP_PROP_WHITE_BALANCE_RED_V, 50) //values from 1-100 with -1 meaning autobalance
+    //not currently implemented yet in library
+    
+    //back to Kaps' original code
+    
+    
+    int nCount = 300;
 
 	
-    //Capturing Frames
-    cout<<"Capturing frames"<<endl;
-    //namedWindow("DisplayImage", WINDOW_NORMAL);
-    //namedWindow("RedImage", WINDOW_NORMAL);
-    //namedWindow("EqualizeImage", WINDOW_NORMAL);
-    //namedWindow("BlurImage", WINDOW_NORMAL);
+    //Capturing Frame
     namedWindow("DISPLAY",WINDOW_NORMAL);
 
     int count = 0;
-    int count2 = 0;
     int x;
     int j;
     
-    ofstream RawData ("Data/Test_Mon_12Dec_Median"+to_string(MBlurRate)+"_Thresh"+to_string(THoldRate)+"BlackPixels.txt");
+    ofstream RawData ("Data/Test_Tue_14Dec/Median"+to_string(MBlurRate)+"_Thresh"+to_string(THoldRate)+"BlackPixels.txt");
     ofstream BlackPixels ("BlackPixels.txt");
-	clock_t Time2;
 
     for(int i = 0; i<=nCount; i++){		  
-    Time2 = clock();
+
 	Camera.grab(); 
 	Camera.retrieve (Image);
-	if(i >= 6){
+	
+	if(i >= 5){
 	    getRedColor(Image);
 
 	    getEqualize(Image);
-
-	    Temp = Image.clone();
 	    
-	    getBlur(Image, MBlurRate);
-	
-	    getThresh(Temp,Temp, THoldRate);
-	    getThresh(Image,Image, THoldRate);
-	  
-		imshow("DISPLAY",Image);
-	    waitKey(1);
-		
-	    for (x = 0; x<Temp.cols;x++){
-		for(j = 0;j<Temp.rows;j++){
-		    int k=Temp.at<uchar>(j,x);
-		    if(k==0){ count2++;}      
-		}
-	    }
+	    getBlur(Image);
 
+	    getThresh(Image);
+	  
 	    for (x = 0; x<Image.cols;x++){
 		for(j = 0;j<Image.rows;j++){
 		    int k=Image.at<uchar>(j,x);
-		    if(k==0){ count++;}      
-		}
+		    if(k == 0){ count++ ;}    
+      		}
 	    } 
 
-
-
-	    if (RawData.is_open()){
+        if (RawData.is_open()){
 		RawData<<"Frame "<<i<<endl;
-		RawData<<"MedianBlur OFF : NOBP = "<<count2<<endl;
-		RawData<<"MedianBlur ON : NOBP = "<<count<<endl;
-		RawData<<"Time :"<<(clock()-Time2)<<endl;
+		//RawData<<"MedianBlur OFF : NOBP = "<<count2<<endl;
+		RawData<<"Black Pixels = "<<count<<endl;
+		double ProcessTime= (clock()-Time);
+		RawData<<"Time :"<<ProcessTime<<endl;
+				   if(BlackPixels.is_open()){
+					    	BlackPixels<<ProcessTime<<" "<<count<<endl;
+					    	//cout<<"in"<<'\n';
+		            }
 		RawData<<'\n';	
 	    }
-	    
-	    if(BlackPixels.is_open()){
-			BlackPixels<<i<<" "<<count<<" "<<count2<<endl;
-		}
-	    
-	    	        
-		cout<<"IMAGE: "<<count<<" in frame "<<i<<endl;
-		cout<<"TEMP: "<<count2<<" in frame "<<i<<'\n';
-		cout<<'\n';
+	              
+		//cout<<"IMAGE: "<<count<<" in frame "<<i<<endl;
+		//cout<<'\n';
 	    count = 0;
-	    count2 = 0;
-	    
+    
 	}
+	
+		imshow("DISPLAY",Image);
+	    waitKey(1);
+	
     }
+    double Total = (clock() - Time)/(double)CLOCKS_PER_SEC;
+    cout<<"Total Process Time "<<setprecision(3)<<(Total)<<endl;
 
     BlackPixels.close();
     RawData.close();
     Camera.release();
-    getHist();   
+  	
+  
 
     //cout<<"Red Process Time "<<RedTime<<'\n';
     //cout<<"Equal Process Time "<<EqualTime<<'\n';
@@ -137,52 +136,49 @@ int THoldRate = 5;
     //double Subtotal = RedTime+EqualTime+BlurTime+ThreshTime;
     //cout<<"Total Process Time "<<setprecision(3)<<(Subtotal)<<endl;
     //cout<<"Stop Camera..."<<endl;
-     
-  	
-    double Total = (clock() - Time)/(double)CLOCKS_PER_SEC;
-    cout<<"Total Process Time "<<setprecision(3)<<(Total)<<endl;
+    getHist();   
     return 0;
 }
 
 void getRedColor(Mat Frame){
-    clock_t start;
-    start = clock();
+    //clock_t start;
+    //start = clock();
     Mat BRG[3];
     split(Frame,BRG);
     Image = BRG[2];
-    double s = ((clock()-start)/(double)CLOCKS_PER_SEC);
-    RedTime += s;
+    //double s = ((clock()-start)/(double)CLOCKS_PER_SEC);
+    //RedTime += s;
 }
 
 void getEqualize (Mat Frame){
-    clock_t start2;
-    start2 = clock();
+    //clock_t start2;
+    //start2 = clock();
     equalizeHist(Frame,Image);
-    double s2 = ((clock()-start2)/(double)CLOCKS_PER_SEC);
-    EqualTime += s2;
+    //double s2 = ((clock()-start2)/(double)CLOCKS_PER_SEC);
+    //EqualTime += s2;
 }
 
-void getBlur (Mat Frame, int rate){
-    clock_t start3;
-    start3 = clock();
-    medianBlur(Frame,Image,rate);
-    double s3 = ((clock()-start3)/(double)CLOCKS_PER_SEC);
-    BlurTime += s3;
+void getBlur (Mat Frame){
+    //clock_t start3;
+    //start3 = clock();
+    medianBlur(Frame,Image,MBlurRate);
+    //double s3 = ((clock()-start3)/(double)CLOCKS_PER_SEC);
+    //BlurTime += s3;
 }
 
-void getThresh (Mat Frame, Mat Frame2, int rate ){
-    clock_t start4;
-    start4 = clock();
-    threshold(Frame,Frame2,rate,255, THRESH_BINARY);
-    double s4 = ((clock()-start4)/(double)CLOCKS_PER_SEC);
-    ThreshTime += s4;
+void getThresh (Mat Frame){
+    //clock_t start4;
+    //start4 = clock();
+    threshold(Frame,Image,THoldRate,255, THRESH_BINARY);
+    //double s4 = ((clock()-start4)/(double)CLOCKS_PER_SEC);
+    //ThreshTime += s4;
 }
 
 void getHist(){	
     FILE *gnuplotPipe = popen ("gnuplot -persistent", "w");
     if(gnuplotPipe){	
-	fprintf(gnuplotPipe, "plot 'BlackPixels.txt' using 1:2 with lines, 'BlackPixels.txt' using 1:3 with lines \n");
-	//fprintf(gnuplotPipe, "plot 'BlackPixels.txt' using 1:2 with lines \n");
+	//fprintf(gnuplotPipe, "plot 'BlackPixels.txt' using 1:2 with lines, 'BlackPixels.txt' using 1:3 with lines \n");
+	fprintf(gnuplotPipe, "plot 'BlackPixels.txt' using 1:2 with lines \n");
 	fflush(gnuplotPipe);
     }
     printf("Displaying graph \n");		
